62462569c5decd42eb91e1d3aa51e5ffc9bbd7b6
kaeeraa <ilhainshakov@yandex.ru>
2025-01-26 22:02:23 +0300

    feat(install): implement install.sh
    Signed-off-by: kaeeraa <ilhainshakov@yandex.ru>

---
+++ install/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+START=$(date +%s)
+echo "#### time-test-facility Install ####"
+echo
+
+if [ "$(dirname "$0")" != "./install" ]; then
+  echo "  Please run this script from the root directory"
+  exit 1
+fi
+
+echo "  Creating ttf-build directory"
+mkdir -p ttf-build
+
+echo "  Hard-linking builds"
+
+linked_count=0
+
+if cp -lf build/main ttf-build/main; then
+  linked_count=$((linked_count + 1))
+else
+  echo "  C++ binary not found"
+fi
+
+if cp -lf src/python/benchmarks.py ttf-build/benchmarks.py; then
+  linked_count=$((linked_count + 1))
+else
+  echo "  Python benchmarks not found"
+fi
+
+if cp -lf src/python/main.py ttf-build/main.py; then
+  linked_count=$((linked_count + 1))
+else
+  echo "  Python main module not found"
+fi
+
+chmod +x ttf-build/main
+
+echo
+echo "  Linked $linked_count files"
+echo
+
+echo "#### time-test-facility Install ####"
+
+END=$(date +%s)
+DIFF=$(($END - $START))
+
+echo
+echo "Install took $DIFF seconds"
