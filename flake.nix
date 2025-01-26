08aaef470d518086d6651fd516c3e3618465fe92
kaeeraa <ilhainshakov@yandex.ru>
2025-01-26 11:36:16 +0300

    feat(nix): implement devshell flake
    Signed-off-by: kaeeraa <ilhainshakov@yandex.ru>

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      pythonEnv = pkgs.python312.withPackages (ps: [
+        ps.numba
+      ]);
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.cmake
+          pkgs.gtest
+          pkgs.python312
+          pythonEnv
+        ];
+
+        shellHook = ''
+          echo "Python version: $(python3.12 --version)"
+
+          mkdir -p build
+          cd build
+          cmake ..
+        '';
+      };
+    };
+}
